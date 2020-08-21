6d929a79385937f247b95b9d99b96c18a9b5058d
katya-sasc <katya-sasc@dataindependence.net>
2020-08-20 19:03:10 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-// alert('Hello world!')
+
+var tdElements = document.getElementsByTagName('TD');
+
+for (var tdElement of thElements) {
+  tdElement.addEventListner('')
+}
+// var tdElements = 
+document. getElementsByTagName('TD');
+ for(let tdElements of tdElements) {
+  //
+ // tdElement.addEventListener('click', function (event)
+}
+//     constant target:
+// HTML.Element = event.target;
+//     target.classList.addEventListener("X")
+//       target.innerHTML = "X" 
+//     })
+// }
