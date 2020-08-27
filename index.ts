3249a43dd453e985c5444a61043c468f2d02dd82
katya-sasc <katya-sasc@dataindependence.net>
2020-08-27 17:58:27 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -1,19 +1,8 @@
 var tdElements = document.getElementsByTagName("TD");
 
-for (var tdElement of thElements) {
-  tdElement.addEventListner("");
+for (var tdElement of tdElements) {
+  tdElement.addEventListener("click", handleClick);
 }
-// var tdElements =
-document.getElementsByTagName("TD");
-for (let tdElements of tdElements) {
-  //
-  // tdElement.addEventListener('click', function (event)
-}
-//     constant target:
-// HTML.Element = event.target;
-//     target.classList.addEventListener("X")
-//       target.innerHTML = "X"
-//     })
-// } 
+
 
 
