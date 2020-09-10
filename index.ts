2a3a8db7ed232e2248fbd391b1c29cc9a01cd58e
katya-sasc <katya-sasc@dataindependence.net>
2020-09-09 19:30:15 -0600

    2 files modified

--- index.ts
+++ index.ts
@@ -3,6 +3,11 @@ var tdElements = document.getElementsByTagName("TD");
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
+  function handleClick(event) {
+  var target = event.target;
+  target.innerHTML = 'X';
+
+}
 
 
 
